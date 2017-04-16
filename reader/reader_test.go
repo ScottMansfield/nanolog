9847892c70064f7e205a939d8c09b54aaa1dc254
Scott Mansfield <sgmansf@gmail.com>
2017-04-16 01:59:06 -0700

    Adding another reader test to exercise failure modes

--- reader/reader_test.go
+++ reader/reader_test.go
@@ -16,7 +16,10 @@ package reader
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
+
+	"io"
 
 	"github.com/ScottMansfield/nanolog"
 )
@@ -40,5 +43,42 @@ func TestReader(t *testing.T) {
 	r := New(inbuf, outbuf)
 	if err := r.Inflate(); err != nil {
 		t.Fatalf("Got error during inflate: %v", err)
+	}
+}
+
+func TestReaderEarlyExits(t *testing.T) {
+	inbuf := &bytes.Buffer{}
+	nanolog.SetWriter(inbuf)
+
+	// This should exercise every different type in one log line
+	h := nanolog.AddLogger("%b %s %i %i8 %i16 %i32 %i64 %u %u8 %u16 %u32 %u64 %f32 %f64 %c64 %c128")
+	nanolog.Log(h,
+		false, "",
+		int(4), int8(4), int16(4), int32(4), int64(4),
+		uint(4), uint8(4), uint16(4), uint32(4), uint64(4),
+		float32(4), float64(4),
+		complex(float32(4), float32(4)), complex(float64(4), float64(4)),
+	)
+	nanolog.Flush()
+
+	outbuf := &bytes.Buffer{}
+
+	for i := 0; i < inbuf.Len(); i++ {
+		t.Run(fmt.Sprintf("Exit%d", i), func(t *testing.T) {
+			defer func() {
+				if r := recover(); r != nil {
+					t.Logf("Panic!: %v", r)
+				} else {
+					t.Logf("No panic.")
+				}
+			}()
+
+			outbuf.Reset()
+			inbufcopy := bytes.NewReader(inbuf.Bytes())
+			r := New(io.LimitReader(inbufcopy, int64(i)), outbuf)
+			if err := r.Inflate(); err != nil {
+				t.Logf("Got error during inflate: %v", err)
+			}
+		})
 	}
 }
